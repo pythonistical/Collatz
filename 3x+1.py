3891327dfd3c4ee560344c49b80df8413c26266f
pythonistical <142655673+pythonistical@users.noreply.github.com>
2024-01-01 14:32:51 +0530

    Update 3x+1.py

--- 3x+1.py
+++ 3x+1.py
@@ -1,16 +1,17 @@
-odd = 0
-even = 0
 current_number = 2
+num = 2
 process = 0
-while process < 30000:
+while process <= 30000:
       process += 1
       if current_number % 2 == 0:
          even_div = current_number/2
+         current_number = current_number/2
          if even_div == 1:
-            current_number += 1
+            num += 1
+            current_number = num
             process = 0
             print(f"Currently working on {current_number}")
       else:
-          odd_mul = (current_number*3) + 1
-print(f'Number {current_number} has been in 3x+1 cycle for more than 30000 times, there is a high chance it might string to infinity')
+          current_number = (current_number*3) + 1
+print(f'Number {num} has been in 3x+1 cycle for more than 30000 times, there is a high chance it might string to infinity')
 
